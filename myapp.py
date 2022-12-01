0ed59f9641cffbefea04ad5e778080721137c1ce
Aniruddh Mantrala <99598258+DarkSamurai1907@users.noreply.github.com>
2022-12-01 12:13:29 +0530

    Add files via upload

--- myapp.py
+++ myapp.py
@@ -16,8 +16,8 @@ tickerDF = tickerdata.history(period='1d', start='2010-5-31', end='2020-5-31')
 st.write("""
 ## Closing Price
 """)
-st.line_chart(tickerDf.Close)
+st.line_chart(tickerDF.Close)
 st.write("""
 ## Volume Price
 """)
-st.line_chart(tickerDf.Volume)
+st.line_chart(tickerDF.Volume)
